ced8d9ee31cf28228f965dd204da3e64356cc3ef
Daniel Quarrell <dan.quarrell73@gmail.com>
2019-10-24 10:16:57 +0100

    Created a simple level generator that generates spikes on onset peaks

--- MusicLevelGenerator/Assets/Scripts/Pre-Processed Example/SongController.cs
+++ MusicLevelGenerator/Assets/Scripts/Pre-Processed Example/SongController.cs
@@ -16,7 +16,10 @@ public class SongController : MonoBehaviour
     [SerializeField] PlotController realTimePlotController;
     */
 
-	int numChannels;
+	[SerializeField] AudioSource audioSource;
+    [SerializeField] LevelGenerator levelGenerator;
+	
+    int numChannels;
 	int numTotalSamples;
 	int sampleRate;
 	float clipLength;
@@ -24,12 +27,9 @@ public class SongController : MonoBehaviour
 	SpectralFluxAnalyzer preProcessedSpectralFluxAnalyzer;
 	PlotController preProcessedPlotController;
     
-	AudioSource audioSource;
 
 	void Start() 
     {
-		audioSource = GetComponent<AudioSource> ();
-
         /*
 		// Process audio as it plays
 		if (realTimeSamples) {
@@ -42,7 +42,7 @@ public class SongController : MonoBehaviour
 
 		// Preprocess entire audio file upfront
 		preProcessedSpectralFluxAnalyzer = new SpectralFluxAnalyzer ();
-		preProcessedPlotController = GameObject.Find ("PreprocessedPlot").GetComponent<PlotController> ();
+		//preProcessedPlotController = GameObject.Find ("PreprocessedPlot").GetComponent<PlotController> ();
 
 		// Need all audio samples.  If in stereo, samples will return with left and right channels interweaved
 		// [L,R,L,R,L,R]
@@ -57,29 +57,24 @@ public class SongController : MonoBehaviour
 		audioSource.clip.GetData(multiChannelSamples, 0);
 		Debug.Log ("GetData done");
 
+        /*
 		Thread bgThread = new Thread (this.GetFullSpectrumThreaded);
 
 		Debug.Log ("Starting Background Thread");
 		bgThread.Start ();
-		
-	}
+		*/
 
-	void Update() 
+        Debug.Log("Starting Background Thread");
+        GetFullSpectrum();
+
+        levelGenerator.GenerateLevelFromSamples(preProcessedSpectralFluxAnalyzer.spectralFluxSamples, audioSource.clip.length);
+        audioSource.Play();
+    }
+
+	void FixedUpdate() 
     {
-        /*
-		// Real-time
-		if (realTimeSamples) 
-        {
-			audioSource.GetSpectrumData (realTimeSpectrum, 0, FFTWindow.BlackmanHarris);
-			realTimeSpectralFluxAnalyzer.AnalyzeSpectrum (realTimeSpectrum, audioSource.time);
-			realTimePlotController.UpdatePlot (realTimeSpectralFluxAnalyzer.spectralFluxSamples);
-		}
-        */
-
-		// Preprocessed
-		int indexToPlot = GetIndexFromTime (audioSource.time) / 1024;
-		preProcessedPlotController.UpdatePlot (preProcessedSpectralFluxAnalyzer.spectralFluxSamples, indexToPlot);
-	}
+        levelGenerator.UpdateLevelPosition(audioSource.time);
+    }
 
 	public int GetIndexFromTime(float curTime) 
     {
@@ -93,7 +88,67 @@ public class SongController : MonoBehaviour
 		return ((1f / (float)this.sampleRate) * index);
 	}
 
-	public void GetFullSpectrumThreaded() 
+    private void GetFullSpectrum()
+    {
+        float[] preProcessedSamples = new float[this.numTotalSamples];
+
+        int numProcessed = 0;
+        float combinedChannelAverage = 0f;
+
+        for (int i = 0; i < multiChannelSamples.Length; i++)
+        {
+            combinedChannelAverage += multiChannelSamples[i];
+
+            // Each time we have processed all channels samples for a point in time, we will store the average of the channels combined
+            if ((i + 1) % this.numChannels == 0)
+            {
+                preProcessedSamples[numProcessed] = combinedChannelAverage / this.numChannels;
+                numProcessed++;
+                combinedChannelAverage = 0f;
+            }
+        }
+
+        Debug.Log("Combine Channels done");
+        Debug.Log(preProcessedSamples.Length);
+        //------------------------------------------
+
+        // Once we have our audio sample data prepared, we can execute an FFT to return the spectrum data over the time domain
+        int spectrumSampleSize = 1024;
+        int iterations = preProcessedSamples.Length / spectrumSampleSize;
+
+        FFT fft = new FFT();
+        fft.Initialize((UInt32)spectrumSampleSize);
+
+        Debug.Log(string.Format("Processing {0} time domain samples for FFT", iterations));
+        double[] sampleChunk = new double[spectrumSampleSize];
+        for (int i = 0; i < iterations; i++)
+        {
+            // Grab the current 1024 chunk of audio sample data
+            Array.Copy(preProcessedSamples, i * spectrumSampleSize, sampleChunk, 0, spectrumSampleSize);
+
+            // Apply our chosen FFT Window
+            double[] windowCoefs = DSP.Window.Coefficients(DSP.Window.Type.Hanning, (uint)spectrumSampleSize);
+            double[] scaledSpectrumChunk = DSP.Math.Multiply(sampleChunk, windowCoefs);
+            double scaleFactor = DSP.Window.ScaleFactor.Signal(windowCoefs);
+
+            // Perform the FFT and convert output (complex numbers) to Magnitude
+            Complex[] fftSpectrum = fft.Execute(scaledSpectrumChunk);
+            double[] scaledFFTSpectrum = DSPLib.DSP.ConvertComplex.ToMagnitude(fftSpectrum);
+            scaledFFTSpectrum = DSP.Math.Multiply(scaledFFTSpectrum, scaleFactor);
+
+            // These 1024 magnitude values correspond (roughly) to a single point in the audio timeline
+            float curSongTime = GetTimeFromIndex(i) * spectrumSampleSize;
+
+            // Send our magnitude data off to our Spectral Flux Analyzer to be analyzed for peaks
+            preProcessedSpectralFluxAnalyzer.AnalyzeSpectrum(Array.ConvertAll(scaledFFTSpectrum, x => (float)x), curSongTime);
+        }
+
+        Debug.Log("Spectrum Analysis done");
+        Debug.Log("Background Thread Completed");
+    }
+
+
+    public void GetFullSpectrumThreaded() 
     {
 		try 
         {
